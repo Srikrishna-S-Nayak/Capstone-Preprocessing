fa5413cdc64724a68c35ffe5c219c0045fc25bdd
Srikrishna S Nayak <srikrishnasnayak@gmail.com>
2024-04-28 20:46:46 +0530

    Try perspective transform

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,4 +1,25 @@
 import cv2
+import numpy as np
+
+x1 = 0
+y1 = 0
+x2 = 0
+y2 = 0
+x3 = 0
+y3 = 0
+x4 = 0
+y4 = 0
+x21 = 100
+y21 = 100
+x22 = 100
+y22 = 100
+x23 = 100
+y23 = 100
+x24 = 100
+y24 = 100
+
+src_points = np.float32([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])  # Specify source points
+dst_points = np.float32([[x21, y21], [x22, y22], [x23, y23], [x24, y24]])  # Specify destination points
 
 
 def gaussian_mixture_model(video_path):
@@ -12,17 +33,12 @@ def gaussian_mixture_model(video_path):
             break
 
         frame = cv2.resize(frame, (640, 360))
-
+        frame = cv2.GaussianBlur(frame, (3, 3), 0)
         back_sub_mask = back_sub.apply(frame)
         back_sub_mask = cv2.morphologyEx(back_sub_mask, cv2.MORPH_OPEN, kernel)
 
-        frame2 = cv2.GaussianBlur(frame, (3, 3), 0)
-        back_sub_mask2 = back_sub.apply(frame2)
-        back_sub_mask2 = cv2.morphologyEx(back_sub_mask2, cv2.MORPH_OPEN, kernel)
-        concatenated_frames = cv2.hconcat([back_sub_mask, back_sub_mask2])
-        cv2.imshow('Foreground Mask Vs FM with GB', concatenated_frames)
-
-        # cv2.imshow('Foreground Mask', back_sub_mask)
+        cv2.imshow('Original video', frame)
+        cv2.imshow('Foreground Mask with Guassian Blurring', back_sub_mask)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -56,12 +72,37 @@ def enhance_video(video_path):
     cv2.destroyAllWindows()
 
 
+def perspective_transform(video_path, src_points, dst_points):
+    vid = cv2.VideoCapture(video_path)
+
+    while True:
+        ret, frame = vid.read()
+        if not ret:
+            print("Could not read video at frame {}".format(frame))
+            break
+        frame = cv2.resize(frame, (640, 360))
+        # Compute perspective transform matrix
+        m = cv2.getPerspectiveTransform(src_points, dst_points)
+
+        # Apply perspective transformation
+        transformed = cv2.warpPerspective(frame, m, (frame.shape[1], frame.shape[0]))
+
+        cv2.imshow('Original video', frame)
+        cv2.imshow('Perspective Transformed', transformed)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    vid.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
 
     vid_path = "./data/D19_20230511115642.mp4"
 
-    choice = input("1 for Gaussian Mixture Model\n2 for Enhance Video \n")
+    choice = input("1 for Gaussian Mixture Model\n2 for Enhance Video \n3 for Perspective Transform\n")
     if choice == "1":
         gaussian_mixture_model(vid_path)
     elif choice == "2":
         enhance_video(vid_path)
+    elif choice == "3":
+        perspective_transform(vid_path, src_points, dst_points)
